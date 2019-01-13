f94e41c8371e4b35e4ae14bb4b8291f8c8753817
Atul <atulbankoti9@gmail.com>
2019-01-13 18:08:39 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='titanic data science challenge',
+    author='Your name (or your organization/company/team)',
+    license='MIT',
+)
